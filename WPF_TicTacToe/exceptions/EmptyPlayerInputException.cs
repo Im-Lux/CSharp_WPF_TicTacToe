49350eb3dd615f654ddd2dc8fbc9f910423ad9ed
Luka Dusak <lux.9833@gmail.com>
2021-08-09 18:07:42 +0200

    Add project files.

---
+++ WPF_TicTacToe/exceptions/EmptyPlayerInputException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_TicTacToe.exceptions
+{
+    class EmptyPlayerInputException : Exception
+    {
+        public EmptyPlayerInputException() { }
+
+        public EmptyPlayerInputException(string message) : base(message) { }
+
+        public EmptyPlayerInputException(string message, Exception exception) : base(message, exception) { }
+
+    }
+}
---
+++ WPF_TicTacToe/model/Player.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace WPF_TicTacToe.model
+{
+    public class Player
+    {
+        private string name;
+        private char value;
+
+        public string Name { get { return name; } set { name = value; } }
+        public char Value { get { return value; } set { this.value = value; } }
+
+        public SolidColorBrush Color { get; set; }
+
+        public Player(string name, char value)
+        {
+            this.Name = name;
+            this.Value = value;
+        }
+    }
+}
